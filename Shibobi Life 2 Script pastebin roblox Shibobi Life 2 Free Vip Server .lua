c6a59f7566e57b6e655909820a07e23f7de228f1
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:55 +0100

    commit zero

---
+++ Shibobi Life 2 Script pastebin roblox Shibobi Life 2 Free Vip Server .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("http://atlas.6te.net/scripts/SL2PSScraper.txt"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+SCRIPT ALL VIP SERVER SHIBOBI LIFE 2, GOOD SCRIPT
+]]
