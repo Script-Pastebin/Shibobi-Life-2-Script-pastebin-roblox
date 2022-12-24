460d7b62deed2f38cd6c19e91bfd8491481ce030
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:44 +0100

    commit zero

---
+++ Shibobi Life 2 Script pastebin roblox Shibobi Life 2 GUI .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Pr6Qh2U4", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							A very good script that can be used now on all Exploits. Many features of AUTO-QUEST, AUTO-FARM, & MORE
+						
+]]
